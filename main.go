d102628862ab74c71bfb120a212ae8631181d2a0
abhishek <abhishek@zipstack.com>
2023-07-05 17:29:35 +0530

    pct-23 initial commit

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+// import (
+// 	"github.com/zipstack/pct-plugin-framework/schema"
+// 	"github.com/zipstack/pct-plugin-framework/server"
+// 	"github.com/zipstack/pct-provider-airflow/plugin"
+// )
+
+// // Set while building the compiled binary.
+// var version string
+
+func main() {
+	// server.Serve(version, plugin.NewProvider, []func() schema.ResourceService{})
+}
